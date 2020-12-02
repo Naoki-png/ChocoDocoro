c9bee7792bd060d35efaf3b292fde05f5c48f608
Naoki-png <ct.anao38@outlook.jp>
2020-12-02 22:55:26 +0900

    [Add] passwordCheck method

--- app/src/main/java/com/example/pien/fragments/createUser/CreateUserFragment.kt
+++ app/src/main/java/com/example/pien/fragments/createUser/CreateUserFragment.kt
@@ -6,14 +6,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pien.R
+import com.example.pien.util.makeToast
+import kotlinx.android.synthetic.main.fragment_create_user.view.*
 
 class CreateUserFragment : Fragment() {
+
+    private var userName: String? = null
+    private var userEmail: String? = null
+    private var userPassword: String? = null
+    private var passwordAgain: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_user, container, false)
+        val view = inflater.inflate(R.layout.fragment_create_user, container, false)
+        userName = view.userName_et.text.toString()
+        userEmail = view.email_et.text.toString()
+        userPassword = view.password_et.text.toString()
+        passwordAgain = view.checkPassword_et.text.toString()
+
+        view.register_btn.setOnClickListener {
+            if (userPassword.equals(passwordAgain)) {
+                registerNewUser(userName, userEmail, userPassword)
+            } else {
+                makeToast(requireContext(), getString(R.string.checkPassword))
+            }
+        }
+        return view
+    }
+
+    private fun registerNewUser(userName: String?, userEmail: String?, userPassword: String?) {
+
     }
 }
--- app/src/main/java/com/example/pien/util/Functions.kt
+++ app/src/main/java/com/example/pien/util/Functions.kt
@@ -1,2 +1,8 @@
 package com.example.pien.util
 
+import android.content.Context
+import android.widget.Toast
+
+fun makeToast(context: Context, msg: String) {
+    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+}
