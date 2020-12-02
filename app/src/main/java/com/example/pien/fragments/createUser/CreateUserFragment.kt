b359f4c79a6a01d8421c4918005f53c1a318bae6
Naoki-png <ct.anao38@outlook.jp>
2020-12-03 08:45:32 +0900

    [Add] createUser method, clearTextField method(for when createUser failed)

--- app/src/main/java/com/example/pien/fragments/createUser/CreateUserFragment.kt
+++ app/src/main/java/com/example/pien/fragments/createUser/CreateUserFragment.kt
@@ -1,20 +1,28 @@
 package com.example.pien.fragments.createUser
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.pien.R
 import com.example.pien.util.makeToast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.fragment_create_user.view.*
 
 class CreateUserFragment : Fragment() {
 
-    private var userName: String? = null
-    private var userEmail: String? = null
-    private var userPassword: String? = null
-    private var passwordAgain: String? = null
+    lateinit var auth: FirebaseAuth
+    lateinit var currentUser: FirebaseUser
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +30,11 @@ class CreateUserFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_create_user, container, false)
-        userName = view.userName_et.text.toString()
-        userEmail = view.email_et.text.toString()
-        userPassword = view.password_et.text.toString()
-        passwordAgain = view.checkPassword_et.text.toString()
+
+        val userName = view.userName_et.text.toString()
+        val userEmail = view.email_et.text.toString()
+        val userPassword = view.password_et.text.toString()
+        val passwordAgain = view.checkPassword_et.text.toString()
 
         view.register_btn.setOnClickListener {
             if (userPassword.equals(passwordAgain)) {
@@ -37,7 +46,37 @@ class CreateUserFragment : Fragment() {
         return view
     }
 
-    private fun registerNewUser(userName: String?, userEmail: String?, userPassword: String?) {
+    /**
+     * アカウント作成メソッド
+     * @param userName 名前
+     * @param userEmail email
+     * @param userPassword パスワード
+     */
+    private fun registerNewUser(userName: String, userEmail: String, userPassword: String) {
+        auth.createUserWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("CREATE USER", "createUserWithEmailAndPassword: succeed")
+                    currentUser = auth.currentUser!!
 
+                    //move to login
+                    findNavController().navigate(R.id.action_createUserFragment_to_loginFragment)
+
+                } else {
+                    Log.w("CREATE USER", "createUserWithEmailAndPassword: failed", task.exception)
+                    makeToast(requireContext(), "create user failed.")
+                    clearTextFields()
+                }
+            }
+    }
+
+    /**
+     * アカウント作成失敗時に、EditTextをクリア
+     */
+    private fun clearTextFields() {
+        requireView().userName_et.setText("")
+        requireView().email_et.setText("")
+        requireView().userName_et.setText("")
+        requireView().userName_et.setText("")
     }
 }
