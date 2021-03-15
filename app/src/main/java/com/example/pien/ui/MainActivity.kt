e64818e95f595ca9b31fe065d42cb5697d0bb030
Naoki-png <ct.anao38@outlook.jp>
2021-03-15 22:14:53 +0900

    Display title on action bar when LoginFragment

--- app/src/main/java/com/example/pien/ui/MainActivity.kt
+++ app/src/main/java/com/example/pien/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
@@ -17,14 +18,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
+                R.id.loginFragment,
                 R.id.listFragment,
                 R.id.postFragment,
                 R.id.favoriteFragment,
@@ -33,6 +37,9 @@ class MainActivity : AppCompatActivity() {
         )
         bottomNavigationView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
